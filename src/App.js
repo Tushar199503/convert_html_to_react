0603d51df030c400852e3781bf2ef406b9febc56
tushar <tushargupta.967@gmail.com>
2021-03-03 17:10:35 +0530

    filesupload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+/* import './App.css'; */
+import Home from './Pages/Home';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import About from './Pages/About';
+import Service from './Pages/Service';
+import Single from './Pages/Single';
+import Gallery from './Pages/Gallery';
+import Contact from './Pages/Contact';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div >
+      <Router>
+      
+      <Header/>
+      <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/about' component={About} />
+      <Route exact path='/service' component={Service} />
+      <Route exact path='/single' component={Single} />
+      <Route exact path='/gallery' component={Gallery} />
+      <Route exact path='/contact' component={Contact} />
+
+      </Switch>
+     
+      <Footer/>
+
+
+      </Router>
+     
+          </div>
   );
 }
 
